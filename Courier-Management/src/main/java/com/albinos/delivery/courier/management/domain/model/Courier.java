18ce281b0397494db1c35802e224418b9b01da49
Pedro Albino <henriquealbino1@gmail.com>
2026-01-22 14:33:34 -0300

    "feature:4.14"

---
+++ Courier-Management/src/main/java/com/albinos/delivery/courier/management/domain/model/Courier.java
@@ -0,0 +1,60 @@
+package com.albinos.delivery.courier.management.domain.model;
+
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.*;
+import java.util.UUID;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+public class Courier {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Setter
+    private String name;
+
+    @Setter
+    private String phone;
+
+    private Integer fulfilledDeliveriesQuantity;
+
+    private Integer pendingDeliveriesQuantity;
+
+    private OffsetDateTime lastFulfilledDeliveryAt;
+
+    private List<AssignedDelivery> pendingDeliveries = new ArrayList<>();
+
+    public List<AssignedDelivery> getPendingDeliveries() {
+        return Collections.unmodifiableList(this.pendingDeliveries);
+    }
+
+    public static Courier brandNew(String name, String phone) {
+        Courier courier = new Courier();
+        courier.setId(UUID.randomUUID());
+        courier.setName(name);
+        courier.setPhone(phone);
+        return courier;
+    }
+
+    public void assing(UUID deliveryId) {
+        this.pendingDeliveries.add(
+                AssignedDelivery.pending(deliveryId)
+        );
+        this.pendingDeliveriesQuantity += 1;
+    }
+
+    public void fulfilledDelivery(UUID deliveryId) {
+        AssignedDelivery delivery = this.pendingDeliveries.stream().filter(d -> d.getId().equals(deliveryId))
+                .findFirst().orElseThrow();
+
+        this.pendingDeliveries.remove(delivery);
+        this.pendingDeliveriesQuantity -= 1;
+        this.fulfilledDeliveriesQuantity += 1;
+        this.lastFulfilledDeliveryAt = OffsetDateTime.now();
+    }
+}
