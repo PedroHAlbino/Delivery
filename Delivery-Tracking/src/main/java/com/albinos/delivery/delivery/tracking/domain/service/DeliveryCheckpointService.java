82d8ca7f49b73777377b34fc3f67e54e680b27b9
Pedro Albino <henriquealbino1@gmail.com>
2026-02-11 22:55:33 -0300

    "feat:6.7"

--- Delivery-Tracking/src/main/java/com/albinos/delivery/delivery/tracking/domain/service/DeliveryCheckpointService.java
+++ Delivery-Tracking/src/main/java/com/albinos/delivery/delivery/tracking/domain/service/DeliveryCheckpointService.java
@@ -1,6 +1,6 @@
 package com.albinos.delivery.delivery.tracking.domain.service;
 
-import com.albinos.delivery.delivery.tracking.api.model.CourierIdInput;
+
 import com.albinos.delivery.delivery.tracking.domain.exception.DomainException;
 import com.albinos.delivery.delivery.tracking.domain.model.Delivery;
 import com.albinos.delivery.delivery.tracking.domain.repository.DeliveryRepository;
@@ -18,19 +18,19 @@ public class DeliveryCheckpointService {
     private final DeliveryRepository deliveryRepository;
 
     public void place(UUID deliveryId) {
-        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(() -> new DomainException());
+        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(DomainException::new);
         delivery.place();
         deliveryRepository.saveAndFlush(delivery);
     }
 
     public void pickup(UUID deliveryId, UUID courierId) {
-        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(() -> new DomainException());
+        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(DomainException::new);
         delivery.pickUp(courierId);
         deliveryRepository.saveAndFlush(delivery);
     }
 
     public void complete(UUID deliveryId) {
-        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(() -> new DomainException());
+        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(DomainException::new);
         delivery.markAsDelivered();
         deliveryRepository.saveAndFlush(delivery);
     }
